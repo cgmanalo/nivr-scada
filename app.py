da530d1cff43c95523eb280e45cfd57d835cafde
cgmanalo <31523001+cgmanalo@users.noreply.github.com>
2026-09-25 06:27:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -285,6 +285,7 @@ HTML_DASHBOARD = """
             <!-- <button class="btn btn-on" onclick="sendCommand('ON')">1200</button> -->
             <button id="btn-1200-M1" class="btn btn-on" style="max-width: 75px;" onclick="sendCommand('TOG-1200-M1')">1200</button>
             <button id="btn-600-M1" class="btn btn-on" style="max-width: 75px;" onclick="sendCommand('TOG-600-M1')">600</button>
+            <button id="btn-300-M1" class="btn btn-on" style="max-width: 75px;" onclick="sendCommand('TOG-300-M1')">300</button>
             <button class="btn btn-off" onclick="sendCommand('OFF')">RELEASE RELAYS / SYSTEM CLEAR</button>
             <div id="status-bar">RELAY OVERRIDE: FETCHING STATE...</div>
         </div>
